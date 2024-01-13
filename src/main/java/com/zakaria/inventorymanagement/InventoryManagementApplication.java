b22084534a1a4248e3d8eab2b0af97eb891694fa
Zakaria <zackelmaoo@hotmail.fr>
2024-01-12 23:59:15 -0500

    (fix): fixed http & https mixed content issue by adding Server URL

--- src/main/java/com/zakaria/inventorymanagement/InventoryManagementApplication.java
+++ src/main/java/com/zakaria/inventorymanagement/InventoryManagementApplication.java
@@ -1,11 +1,12 @@
 package com.zakaria.inventorymanagement;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 @EnableJpaAuditing
 public class InventoryManagementApplication {
@@ -13,7 +14,7 @@ public class InventoryManagementApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryManagementApplication.class, args);
-		System.out.println("Hello World");
+		System.out.println("<== Inventory Management System: Started ==>");
 	}
 	
 	
